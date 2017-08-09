ab45f78f28fb7457567237ef26d59c34e0e8363e
Camden Bickel <cam@bickel.io>
2017-08-09 12:05:36 -0700

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+// Application entrypoint.
+
+// Render the top-level React component
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('react-root'));
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    './src/index.jsx'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/build/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel-loader'],
+        include: path.join(__dirname, 'src')
+      }
+    ]
+  }
+}
