c85e87facb8d664ab24e69d58ea1f03c81dd6d3b
Camden Bickel <cam@bickel.io>
2017-08-09 13:22:59 -0700

    Added watching

--- client/App.jsx
+++ client/App.jsx
@@ -1,10 +1,8 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return (
-      <h1>Hello React :)</h1>
-    );
+    return <h1>Yo React :)</h1>;
   }
 }
 export default App;
