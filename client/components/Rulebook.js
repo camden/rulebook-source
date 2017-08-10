2cc2c1b00e25c0e528b626d521d9ef0e329f310a
Camden Bickel <cam@bickel.io>
2017-08-09 17:35:38 -0700

    Added proptypes

--- client/components/Rulebook.js
+++ client/components/Rulebook.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { fetchRulebookData } from 'utils';
 
 export default class Rulebook extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    fetchRulebookData(this.props.match.params.rulebookName);
+  }
+
   render() {
     return (
       <div>
-        {this.props.match.params.rulebookName}
+        This is the rulebook for {this.props.match.params.rulebookName}
       </div>
     );
   }
 }
+
+Rulebook.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      rulebookName: PropTypes.string,
+    }),
+  }).isRequired,
+};
