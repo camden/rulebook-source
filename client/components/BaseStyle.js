252fe1382cdb33fa9ab56038626fe0dd6a43a5e1
Camden Bickel <cam@bickel.io>
2017-08-11 12:01:48 -0700

    Added Open Sans as base font

--- client/components/BaseStyle.js
+++ client/components/BaseStyle.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const StyledDiv = styled.div`font-family: Monaco;`;
+const StyledDiv = styled.div`font-family: 'Open Sans', sans;`;
 
 const BaseStyle = ({ children }) => {
   return (
