7c46e4b47b7d31865d3349ca581c7b44c5d8c945
Camden Bickel <cam@bickel.io>
2017-08-13 12:13:06 -0700

    Added anchor link

--- client/markdown-utils.js
+++ client/markdown-utils.js
@@ -2,6 +2,25 @@
 
 import React, { createElement } from 'react';
 import marksy from 'marksy';
+import { Link } from 'react-router-dom';
+import { Link as LinkIcon } from 'react-feather';
+import styled from 'styled-components';
+
+const AnchorLink = styled(LinkIcon)`
+  color: #ccc;
+  padding-right: 0.5rem;
+  transition: all 150ms linear;
+
+  &:hover {
+    cursor: pointer;
+    color: #888;
+  }
+`;
+
+const Header = styled.h1`
+  font-size: ${props => props.size}rem;
+  display: inline-block;
+`;
 
 export const compileMarkdown = marksy({
   // Pass in whatever creates elements for your
@@ -13,9 +32,14 @@ export const compileMarkdown = marksy({
   elements: {
     h1({ id, children }) {
       return (
-        <h1>
-          {children}
-        </h1>
+        <div>
+          <Link to={`#${id}`}>
+            <AnchorLink size={20} />
+          </Link>
+          <Header id={id} size={2}>
+            {children}
+          </Header>
+        </div>
       );
     },
   },
