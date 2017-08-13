bd557f4e3f9d8864a5704274344732ccb000884a
Camden Bickel <cam@bickel.io>
2017-08-13 12:34:24 -0700

    Finished links

--- client/markdown-utils.js
+++ client/markdown-utils.js
@@ -20,7 +20,7 @@ const AnchorLink = styled(LinkIcon)`
 const GenericHeader = styled.div`
   font-size: ${props => props.size};
   font-weight: bold;
-  margin: 0.5px 0;
+  margin: 0.5rem 0;
   display: inline-block;
 `;
 
@@ -37,9 +37,9 @@ const Header = ({ id, level, children }) => {
   return (
     <div>
       <Link to={`#${id}`}>
-        <AnchorLink size={level * 15} />
+        <AnchorLink size={14} />
       </Link>
-      <GenericHeader size={levelToSizeMap[level.toString()]}>
+      <GenericHeader id={id} size={levelToSizeMap[level.toString()]}>
         {children}
       </GenericHeader>
     </div>
@@ -56,11 +56,44 @@ export const compileMarkdown = marksy({
   elements: {
     h1({ id, children }) {
       return (
-        <div>
-          <Header id={id} level={1}>
-            {children}
-          </Header>
-        </div>
+        <Header id={id} level={1}>
+          {children}
+        </Header>
+      );
+    },
+    h2({ id, children }) {
+      return (
+        <Header id={id} level={2}>
+          {children}
+        </Header>
+      );
+    },
+    h3({ id, children }) {
+      return (
+        <Header id={id} level={3}>
+          {children}
+        </Header>
+      );
+    },
+    h4({ id, children }) {
+      return (
+        <Header id={id} level={4}>
+          {children}
+        </Header>
+      );
+    },
+    h5({ id, children }) {
+      return (
+        <Header id={id} level={5}>
+          {children}
+        </Header>
+      );
+    },
+    h6({ id, children }) {
+      return (
+        <Header id={id} level={6}>
+          {children}
+        </Header>
       );
     },
   },
