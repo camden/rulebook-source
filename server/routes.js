fa237f4c547a130e98bf0a9cf34fa5d9e46ff51c
Camden Bickel <cam@bickel.io>
2017-08-13 17:46:38 -0700

    Fixed cache

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,6 @@
 // @flow
 
-import { getMarkdownForRulebook } from './utils';
+import { getRulebookContent } from './utils';
 
 export const addRoutes = ({ router, redis }) => {
   if (!redis) {
@@ -10,19 +10,29 @@ export const addRoutes = ({ router, redis }) => {
   router.route('/rulebooks/:rulebookName').get((req, res) => {
     const rulebookName = req.params.rulebookName;
 
-    getMarkdownForRulebook(rulebookName).then(markdownResponse => {
+    getRulebookContent(rulebookName).then(markdownResponse => {
       if (markdownResponse.status === 404) {
         return res.status(markdownResponse.status).json({
           message: `Rulebook ${rulebookName} not found.`,
         });
       }
 
-      res.status(markdownResponse.status);
+      if (markdownResponse.status !== 200) {
+        return res.status(markdownResponse.status).json({
+          message: 'Unknown error.',
+        });
+      }
 
-      redis.set(req.url, markdownResponse.data.content);
+      if (!markdownResponse.encodedContent) {
+        return res.status(500).json({
+          message: 'markdownResponse must have encodedContent attribute.',
+        });
+      }
+
+      redis.setex(req.url, 3600, markdownResponse.encodedContent);
 
       return res.json({
-        data: markdownResponse.data.content,
+        data: markdownResponse.encodedContent,
       });
     });
   });
--- server/utils.js
+++ server/utils.js
@@ -1,40 +1,21 @@
 // @flow
-import ghrequest from 'ghrequest';
+import fetch from 'node-fetch';
 
 const REPO_AUTHOR = 'camden';
 const REPO_NAME = 'rulebooks';
 const GITHUB_ROOT = 'https://api.github.com';
 const GITHUB_API_URL = `/repos/${REPO_AUTHOR}/${REPO_NAME}/contents`;
 
-const getRulebookContent = async (rulebookName: string): Promise<Object> => {
-  return new Promise((resolve, reject) => {
-    ghrequest(
-      {
-        url: GITHUB_API_URL + '/rulebooks/' + rulebookName,
-        headers: {
-          'User-Agent': REPO_AUTHOR,
-        },
-      },
-      (error, response, body) => {
-        if (error) {
-          return reject(error);
-        }
-
-        return resolve(response);
-      }
-    );
-  });
-};
-
-export const getMarkdownForRulebook = async (
+export const getRulebookContent = async (
   rulebookName: string
-): Promise<*> => {
-  const rulebookDataResponse = await getRulebookContent(rulebookName);
-  const rulebookData = await rulebookDataResponse.toJSON();
+): Promise<Object> => {
+  const url = GITHUB_ROOT + GITHUB_API_URL + '/rulebooks/' + rulebookName;
+  const response = await fetch(url);
+  const rulebookData = await response.json();
 
   return {
-    status: rulebookData.statusCode,
-    data: rulebookData.body,
+    status: response.status,
+    encodedContent: rulebookData.content,
   };
 };
 
