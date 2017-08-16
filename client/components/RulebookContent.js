803751d342afa16eb40c8644b7fd78ef548ade4c
Camden Bickel <cam@bickel.io>
2017-08-15 20:15:17 -0700

    Added react-motion

--- client/components/RulebookContent.js
+++ client/components/RulebookContent.js
@@ -1,19 +1,41 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { spring, Motion } from 'react-motion';
 
 const HoveredDiv = styled.div`
   padding: 1rem 3rem;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.24);
+  box-shadow: 0px 4px 14px rgba(0, 0, 0, 0.24);
+  transform: translate3d(${props => props.translate}%, 0, 0);
 `;
 
-const RulebookContent = ({ markdown }) => {
-  return (
-    <HoveredDiv>
-      {markdown}
-    </HoveredDiv>
-  );
+class RulebookContent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpen: false,
+    };
+  }
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <Motion style={{ translate: spring(this.state.isOpen ? -10 : 0) }}>
+        {interpolatedStyle =>
+          <HoveredDiv translate={interpolatedStyle.translate}>
+            {this.props.markdown}
+          </HoveredDiv>}
+      </Motion>
+    );
+  }
+}
+
+RulebookContent.propTypes = {
+  markdown: PropTypes.node.isRequired,
 };
 
 export default RulebookContent;
