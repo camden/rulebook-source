96d0ae80ff4cb78e2bd7d020753b9d302eb4847d
Camden Bickel <cam@bickel.io>
2017-08-26 13:20:29 -0700

    Added addtl compressing

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,20 @@ module.exports = {
         NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'production'),
       },
     }),
+    new webpack.optimize.UglifyJsPlugin({
+      comments: false, // remove comments
+      compress: {
+        unused: true,
+        dead_code: true, // big one--strip code that will never execute
+        warnings: false, // good for prod apps so users can't peek behind curtain
+        drop_debugger: true,
+        conditionals: true,
+        evaluate: true,
+        drop_console: true, // strips console statements
+        sequences: true,
+        booleans: true,
+      },
+    }),
     new HtmlWebpackPlugin({
       template: 'client/index.html',
     }),
