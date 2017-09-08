463a244d7af1242ca895b06f2fc05f22a502be0c
Camden Bickel <cam@bickel.io>
2017-09-07 21:55:57 -0400

    Added prerender

--- server/main.js
+++ server/main.js
@@ -5,6 +5,7 @@ import 'babel-polyfill';
 import dotenv from 'dotenv';
 import express from 'express';
 import bodyParser from 'body-parser';
+import prerender from 'prerender-node';
 import path from 'path';
 import redis from 'redis';
 
@@ -13,6 +14,9 @@ import { addRoutes } from './routes';
 
 // Setup env vars
 dotenv.config();
+if (!process.env.PRERENDER_TOKEN) {
+  throw new Error('Prerender token must be set.');
+}
 
 process.title = process.argv[2];
 
@@ -30,6 +34,7 @@ redisClient.on('connect', () => {
 });
 
 const main = () => {
+  app.use(prerender.set('prerenderToken', process.env.PRERENDER_TOKEN));
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
 
