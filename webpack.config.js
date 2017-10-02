9b2321353a60702b7d4dd0714f526b5f4566881c
Camden Bickel <cam@bickel.io>
2017-10-02 19:29:32 +0100

    Fixed string issue

--- webpack.config.js
+++ webpack.config.js
@@ -65,7 +65,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(['dist', 'tmp']),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': process.env.NODE_ENV,
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
     }),
     new Dotenv({
       path: './.env',
